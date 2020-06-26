1e1d6aeb7b9c763bedd2638342b9ae047d8a7130
yuyao <980903093@qq.com>
2020-06-26 09:10:23 +0800

    第一个webgl，清空绘图区

---
+++ HelloCanvas/HelloCanvas.js
@@ -0,0 +1,15 @@
+function main(){
+  //获取canvas元素
+  var canvas = document.getElementById('webgl');
+  //获取WebGl绘图上下文
+  var gl = getWebGLContext(canvas);
+  if(!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+ //指定清空canvas的颜色
+ gl.clearColor(0.5, 0.5, 0.5, 1.0);
+
+ //清空canvas
+gl.clear(gl.COLOR_BUFFER_BIT);
+}
